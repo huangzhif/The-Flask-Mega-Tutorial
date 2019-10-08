3cd63fa31519ce04859bb998e42cd0ea6eac055c
root <root@localhost.localdomain>
2019-10-09 06:54:50 +0800

    第四章：数据库

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'app.db')
+    # 设置数据发生变更后是否发送信号给应用
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
