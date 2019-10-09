69cfd748e2c7b15f230298050424fb3cb087d7e3
root <root@localhost.localdomain>
2019-10-10 07:21:05 +0800

    第六章：个人主页和头像

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 from app import db,login
@@ -11,11 +12,18 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post',backref='author',lazy='dynamic')
 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime,default=datetime.utcnow)
+
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self,password):
         return check_password_hash(self.password_hash,password)
+    
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
 
     def __repr__(self):
