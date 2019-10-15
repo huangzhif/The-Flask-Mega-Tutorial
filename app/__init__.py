140b60a06b6b083b057c0f21eee7a69bd2c8ea35
root <root@localhost.localdomain>
2019-10-15 11:57:53 +0800

    第十一章：美化(v0.11)

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__,instance_relative_config=True)
@@ -11,6 +12,8 @@ app.config.from_object(Config)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+
+bootstrap = Bootstrap(app)
 
 mail = Mail(app)
 
